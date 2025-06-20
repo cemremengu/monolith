7858994889345870a89fe41536f495fa8bf5f154
cemre.mengu <cemre.mengu@ttgint.com>
2025-06-20 16:36:25 +0300

    refactor account table

---
+++ migrations/001_create_account_table.sql
@@ -0,0 +1,22 @@
+-- +goose Up
+CREATE TABLE account
+(
+    id           UUID                     default gen_random_uuid() PRIMARY KEY,
+    username     TEXT                                   NOT NULL,
+    email        TEXT UNIQUE                            NOT NULL,
+    name         TEXT,
+    password     TEXT,
+    salt         TEXT,
+    rands        TEXT,
+    is_admin     BOOLEAN                  DEFAULT FALSE,
+    language     TEXT,
+    theme        TEXT,
+    timezone     TEXT,
+    last_seen_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    is_disabled  boolean                  default FALSE not null,
+    created_at   TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at   TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+-- +goose Down
+DROP TABLE account;
